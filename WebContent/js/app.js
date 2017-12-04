3919988f2e24d093ea3203231d4a15778457dbc4
Gabriel Jacquier <gabrieljacquierme@gmail.com>
2017-12-03 23:50:09 -0200

    Adiciona estrutura para request de atividades

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,18 +1,65 @@
 angular.module('MeiApp', [])
   .controller('MeiController', function($scope, $http) {
-	  $scope.atividades = ["aaaa", "sdf", "sdfs"];
-	  $scope.descricao = "";
+	  
+	  $scope.trocarDescricao = function() {
+		  $scope.descricao = "";
+		  $scope.descricaoCheck = true;
+		  $scope.ocupacaoCheck = false;
+		  $scope.codigoCheck = false;
+	  }
+	  
+	  $scope.trocarOcupacao = function() {
+		  $scope.descricao = "";
+		  $scope.descricaoCheck = false;
+		  $scope.ocupacaoCheck = true;
+		  $scope.codigoCheck = false;
+	  }
+	  
+	  $scope.trocarCodigo = function() {
+		  $scope.descricao = "";
+		  $scope.descricaoCheck = false;
+		  $scope.ocupacaoCheck = false;
+		  $scope.codigoCheck = true;
+	  }
+
+	  $scope.trocarDescricao();
 
 	  $scope.buscar = function() {
 		  var param = $scope.descricao;
-		  $http({
-			    url: "http://localhost:8080/tcc/atividade/descricao", 
-			    method: "GET",
-			    params: {"descricao": param}
-			 })
-		  .then(function(response) {
-			  console.log(response.data);
-			  $scope.atividades = response.data.atividadesEncontradas;
-		  });
+		  if($scope.descricaoCheck) {
+			  $http({
+				    url: "http://localhost:8080/tcc/atividade/descricao", 
+				    method: "GET",
+				    params: {"descricao": param}
+				 })
+			  .then(function(response) {
+				  console.log(response.data);
+				  $scope.atividadesDescricao = response.data.atividadesEncontradas;
+			  });
+		  }
+
+		  if($scope.ocupacaoCheck) {
+			  $http({
+				    url: "http://localhost:8080/tcc/cnae", 
+				    method: "GET",
+				    params: {"ocupacao": param}
+				 })
+			  .then(function(response) {
+				  console.log(response.data);
+				  $scope.atividadesOcupacao = response.data;
+			  });
+		  }
+
+		  if($scope.codigoCheck) {
+			  $http({
+				    url: "http://localhost:8080/tcc/atividade/cnae", 
+				    method: "GET",
+				    params: {"cnae": param}
+				 })
+			  .then(function(response) {
+				  console.log(response.data);
+				  $scope.atividadesCodigo = response.data;
+			  });
+		  }
 	  }
   });
